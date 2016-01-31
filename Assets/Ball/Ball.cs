42e16ddb716600036908249010bf8627fee7f73e
Jimmy Pocock <jimmypocock@yahoo.com>
2016-01-30 20:36:23 -0600

    Created Camera Control to follow ball on launch

---
+++ Assets/Ball/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+	public Vector3 launchVelocity;
+
+	private Rigidbody rigidbody;
+	private AudioSource audioSource;
+
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+
+	public void Launch ()
+	{
+		rigidbody.velocity = launchVelocity;
+		audioSource.Play ();
+	}
+}
